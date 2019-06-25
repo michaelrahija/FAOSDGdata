91c40af92b7195eb7227c50c1bfa7bd38deaf58e
= <=>
2019-06-25 15:10:13 +0200

    first commit

---
+++ R/getData.R
@@ -0,0 +1,52 @@
+#' getData
+#' This is function which takes as an input an SDG indicator, and returns
+#' the latest dataset for that indicator.
+#'
+#' @param sdg is a specific SDG indicator under FAO custodianship
+#'
+#' @return This function returns a dataframe containing the latest published data
+#'
+#' @export
+
+getData <- function(sdg = c("2.1.1",
+                            "2.1.2",
+                            "2.3.1",
+                            "2.3.2",
+                            "2.5.2",
+                            "2.a.1",
+                            "2.c.1",
+                            "6.4.1",
+                            "6.4.2",
+                            "14.4.1",
+                            "14.6.1",
+                            "14.b.1",
+                            "15.1.1",
+                            "15.2.1",
+                            "15.4.2",
+                            "15.6.1")){
+
+  #checks
+  if(length(sdg) > 1) stop("Only one SDG indicator may be retrieved at a time")
+
+  #build uRL
+  base_url <- sprintf("http://sdlc.fao.org/artifactory/fao-sdg-releases/%s/", sdg)
+  mid_url <- gsub("\\.", "_", sdg)
+  end_url <- paste0("_","DataExport_3_2019.xls")
+
+  url <- paste0(base_url, mid_url,end_url)
+
+  #download SDG file
+  dl_file <- tempfile(fileext = ".xls")
+
+  download.file(url,
+                destfile = dl_file,
+                mode  = "wb",
+                quiet = T)
+
+  df <- readxl::read_xls(dl_file,
+                         sheet = 1)
+
+
+df
+
+}
