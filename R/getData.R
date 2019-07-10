9c720a241c47deca604a51be7e9864b344190143
= <=>
2019-07-10 17:06:53 +0200

    added local option to get data

--- R/getData.R
+++ R/getData.R
@@ -1,11 +1,12 @@
 #' getData
 #' This is function which takes as an input an SDG indicator, and returns
-#' the latest dataset for that indicator.
+#' the latest dataset for that indicator as a dataframe.
 #'
 #' @param sdg is a specific SDG indicator under FAO custodianship
 #'
 #' @return This function returns a dataframe containing the latest published data
 #'
+#'#' @importFrom readxl read_xls
 #' @export
 
 getData <- function(sdg = c("2.1.1",
@@ -25,46 +26,75 @@ getData <- function(sdg = c("2.1.1",
                             "15.1.1",
                             "15.2.1",
                             "15.4.2",
-                            "15.6.1")){
+                            "15.6.1"),
+                    source = c("web",'local')){
 
   #checks
   if(length(sdg) > 1) stop("Only one SDG indicator may be retrieved at a time")
 
   #build uRL
   
-  #custom URL for 2.5.1 plants
-  if(sdg == "2.5.1_Plants"){
-    url <-"http://sdlc.fao.org/artifactory/fao-sdg-releases/2.5.1%20plants/2_5_1_Plants_DataExport_3_2019.xls" 
+  if(source == "web"){
+    #custom URL for 2.5.1 plants
+    if(sdg == "2.5.1_Plants"){
+      url <-"http://sdlc.fao.org/artifactory/fao-sdg-releases/2.5.1%20plants/2_5_1_Plants_DataExport_3_2019.xls" 
+      
+    } else if (sdg == "2.5.1_Animals"){
+      url <- "http://sdlc.fao.org/artifactory/fao-sdg-releases/2.5.1%20animals/2_5_1_Animals_DataExport_3_2019.xls"
+      
+    } else if (sdg %in% c("6.4.1", "6.4.2")){
+      base_url <- sprintf("http://sdlc.fao.org/artifactory/fao-sdg-releases/%s/", sdg)
+      mid_url <- gsub("\\.", "_", sdg)
+      end_url <- paste0("_","DataExport_6_2019.xls")
+      url <- paste0(base_url, mid_url,end_url)
+      
+    } else {
+      base_url <- sprintf("http://sdlc.fao.org/artifactory/fao-sdg-releases/%s/", sdg)
+      mid_url <- gsub("\\.", "_", sdg)
+      end_url <- paste0("_","DataExport_3_2019.xls")
+      url <- paste0(base_url, mid_url,end_url)
+      
+    }
+  
+    #download SDG file
+    dl_file <- tempfile(fileext = ".xls")
+  
+    download.file(url,
+                  destfile = dl_file,
+                  mode  = "wb",
+                  quiet = T)
     
-  } else if (sdg == "2.5.1_Animals"){
-    url <- "http://sdlc.fao.org/artifactory/fao-sdg-releases/2.5.1%20animals/2_5_1_Animals_DataExport_3_2019.xls"
+    #extract dataset, and version
+    df <- readxl::read_xls(dl_file,
+                           sheet = "data")
     
-  } else if (sdg %in% c("6.4.1", "6.4.2")){
-    base_url <- sprintf("http://sdlc.fao.org/artifactory/fao-sdg-releases/%s/", sdg)
-    mid_url <- gsub("\\.", "_", sdg)
-    end_url <- paste0("_","DataExport_6_2019.xls")
-    url <- paste0(base_url, mid_url,end_url)
+    version <-readxl::read_xls(dl_file,
+                               sheet = "version")
     
-  } else {
-    base_url <- sprintf("http://sdlc.fao.org/artifactory/fao-sdg-releases/%s/", sdg)
-    mid_url <- gsub("\\.", "_", sdg)
-    end_url <- paste0("_","DataExport_3_2019.xls")
-    url <- paste0(base_url, mid_url,end_url)
+  } else{
     
+    files.full <- list.files("data/", full.names = T)
+    sdg.name <- gsub(pattern = "\\.",
+                     replacement = "_",
+                     sdg)
+    
+    file.to.read <- files.full[grep(sdg.name,files.full)]
+    
+    df <- readxl::read_xls(file.to.read, 
+                           sheet = "data")
+    
+    version <-readxl::read_xls(file.to.read,
+                               sheet = "version")
+                           
   }
-
-  #download SDG file
-  dl_file <- tempfile(fileext = ".xls")
-
-  download.file(url,
-                destfile = dl_file,
-                mode  = "wb",
-                quiet = T)
-
-  df <- readxl::read_xls(dl_file,
-                         sheet = 1)
-
+  
+  
+  #add version column
+  version <- colnames(version)[1]
+  df$version <- version
 
 df
 
 }
+
+
--- testfunctions.R
+++ testfunctions.R
@@ -1,4 +1,5 @@
 #test functions
+library(dplyr)
 
 source("R/getData.R")
 
@@ -21,15 +22,44 @@ sdgs <- c("2.1.1",
           "15.4.2")
           #"15.6.1") #18
 
-y <- getData("2.1.1")
+
+y <- lapply(sdgs, function(x) getData(x, source = "web"))
+
+x <- y
+
+
+#harmonize column names
+lapply(x, colnames)
 
 
 
-x <- lapply(sdgs, getData)
+names(x) <- lapply(x, function(x) unique(x$Indicator))
+
+#need to edit 2.5.1 name, a = plants, b = animals
+temp <- x[names(x) == '2.5.1']
+
+animals <- sapply(temp, function(x) grep("Plant",x$SeriesDescription))
 
 
 
-names(x) <- gsub("\\.", "_",sdgs)
-sapply(x,colnames)
+lapply(x, function(x) unique(x$SeriesDescription))
 
-sapply(x, head)
+#datasets for `15.2.1`, `2.5.2`
+
+
+lapply(x, colnames)
+
+#2.5.1 Indicator Reference Number should be renamed to "Indicator"
+#6.4.1 file contains indicator values that are not only 6.4.1
+
+names <- lapply(x, colnames)
+
+indicator.test <- list()
+for(i in 1:length(y)){
+  
+  y[[i]]$indicator
+  
+}
+
+
+
