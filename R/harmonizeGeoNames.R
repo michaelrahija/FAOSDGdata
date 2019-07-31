ca80517953c4589f2d616a283e43f2e1410428d6
Michael Rahija <michaelrahija@Michaels-MacBook-Pro-2.local>
2019-07-31 20:37:44 +0200

    use merge instead of left_join for m49, unfinished name replacing

--- R/harmonizeGeoNames.R
+++ R/harmonizeGeoNames.R
@@ -9,6 +9,7 @@
 #'
 #' @importFrom dplyr left_join
 #' @importFrom tidyselect everything
+#' @importFrom stringr str_length
 #' @export
 
 
@@ -18,18 +19,35 @@ harmonizeGeoNames <- function(cleanSdgs){
   if(!is.data.frame(cleanSdgs)) stop("must be a dataframe")
   if(!("areacode" %in% colnames(cleanSdgs))) stop("areacode column not found")
   
-  #clean codes in cleanSdgs
+  #add 0s to codes for sdg dataset
   cleanSdgs$areacode[str_length(cleanSdgs$areacode) == 1] <- paste0("00", cleanSdgs$areacode[str_length(cleanSdgs$areacode) == 1])
   cleanSdgs$areacode[str_length(cleanSdgs$areacode) == 2] <- paste0("0", cleanSdgs$areacode[str_length(cleanSdgs$areacode) == 2])
   
   #add official m49 name column
-  cleanSdgs <- left_join(cleanSdgs, 
-                  m49, #internal dataframe
-                  by = c("areacode" = "Code"))
+  # cleanSdgs <- left_join(cleanSdgs, 
+  #                           m49, #internal dataframe
+  #                           by = c("areacode" = "Code"))
+  
+  cleanSdgs_m <- merge(cleanSdgs,
+                     m49,
+                     by.x = "areacode",
+                     by.y = "Code",
+                     all = T)
+  
+  #remove rows with NA areanames, result of merger w/ M49. Some M49 names, didn't have codes
+  cleanSdgs_m <- cleanSdgs_m[!is.na(cleanSdgs_m$areaname),]
+  
+  #some m49 areas are combined, and sometimes it says "(M49)" sometimes, not
+  test <- filter(cleanSdgs_m, is.na(Name))
+  test %>% group_by(areacode, areaname) %>% summarize(n = n())
+  
+  test2 <- gsub(pattern = " (M49) ",
+                "",
+                test)
   
   #since some areas are combination with no official, take areaname if Name is NA
-  cleanSdgs$Name[is.na(cleanSdgs$Name)] <- cleanSdgs$areaname[is.na(cleanSdgs$Name)]
-  cleanSdgs <- cleanSdgs %>%
+  cleanSdgs_m$Name[is.na(cleanSdgs_m$Name)] <- cleanSdgs_m$areaname[is.na(cleanSdgs_m$Name)]
+  cleanSdgs_m <- cleanSdgs_m %>%
     select(-areaname) %>%
     rename(areaname = Name) %>%
     select(areacode, areaname, everything())
