a252a63e4c73284d812da9fc7b71f90c8ac2d267
Michael Rahija <michaelrahija@Michaels-MBP-2.lan>
2019-08-09 07:07:33 +0200

    added FAO classifications

--- R/adopt_m49_for_FAO.R
+++ R/adopt_m49_for_FAO.R
@@ -38,23 +38,89 @@ adopt_m49_for_FAO <- function(raw_m49){
     "develop"
   )
   
+  raw_m49$footnote = "unaltered M49"
   #custom FAO classifications
-  taiwan <- data.frame(areacode = "158",
-                       areaname = "Taiwan, Province of China",
-                       footnote  = "FAO code and descriptor; descriptor as in NOCS")
-  tanzania <- data.frame(areacode = "836",
-                         areaname = "United Republic of Tanzania, Zanzibar",
-                         footnote  = "FAO code and descriptor; agreed with FAOTERM")
-  kosovo <- data.frame(areacode = "412",
-                         areaname = "Kosovo (Serbia)",
-                         footnote  = "FAO code and descriptor; agreed with FAOTERM")
+  taiwan <- data.frame(
+    globalcode = "1",
+    globalname = "World",
+    regioncode = "142",
+    regionname = "Asia",
+    subregioncode = "30",
+    subregionname = "Eastern Asia",
+    intermediateregioncode = NA,
+    intermediateregionname = NA,
+    areacode = "158",
+    areaname = "Taiwan, Province of China",
+    iso3 = "",
+    ldc = NA,
+    lldc = NA,
+    sids = NA,
+    develop = NA,
+    footnote  = "FAO code and descriptor; descriptor as in NOCS"
+  )
   
-  palestine <- data.frame(areacode = "275",
-                          areaname = "Palestine",
-                          footnote  = "FAO descriptor as in NOCS")
+  tanzania <- data.frame(
+    globalcode = "1",
+    globalname = "World",
+    regioncode = "2",
+    regionname = "Africa",
+    subregioncode = "201",
+    subregionname = "Sub-Saharan Africa",
+    intermediateregioncode = "14",
+    intermediateregionname = "Eastern Africa",
+    areacode = "836",
+    areaname = "United Republic of Tanzania, Zanzibar",
+    iso3 = "",
+    ldc = NA,
+    lldc = NA,
+    sids = NA,
+    develop = NA,
+    footnote  = "FAO code and descriptor; agreed with FAOTERM"
+  )
+  kosovo <- data.frame(
+    globalcode = "1",
+    globalname = "World",
+    regioncode = "150",
+    regionname = "Europe",
+    subregioncode = "39",
+    subregionname = "Southern Europe",
+    intermediateregioncode = NA,
+    intermediateregionname = NA,
+    areacode = "412",
+    areaname = "Kosovo (Serbia)",
+    iso3 = "",
+    ldc = NA,
+    lldc = NA,
+    sids = NA,
+    develop = NA,
+    footnote  = "FAO code and descriptor; agreed with FAOTERM"
+  )
   
-
+  palestine <- data.frame(
+    globalcode = "1",
+    globalname = "World",
+    regioncode = "142",
+    regionname = "Asia",
+    subregioncode = "145",
+    subregionname = "Western Asia",
+    intermediateregioncode = "",
+    intermediateregionname = "",
+    areacode = "275",
+    areaname = "Palestine",
+    iso3 = "",
+    ldc = NA,
+    lldc = NA,
+    sids = NA,
+    develop = NA,
+    footnote  = "FAO descriptor as in NOCS. Code same as M49, but name changed."
+  )
+  
+  m49 <- rbind(raw_m49,
+               taiwan,
+               tanzania,
+               kosovo,
+               palestine)
   
   
-  
+m49  
 }
